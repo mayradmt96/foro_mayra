289b59f630db215aa49a4b496709b4f4bd56b2c2
Mayra <mayradmt96@gmail.com>
2020-02-05 00:28:36 +0100

    añadido entity Publicacion

---
+++ src/Entity/Comentario.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\ComentarioRepository")
+ */
+class Comentario
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $fecha_publicacion;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $contenido;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getFechaPublicacion(): ?\DateTimeInterface
+    {
+        return $this->fecha_publicacion;
+    }
+
+    public function setFechaPublicacion(\DateTimeInterface $fecha_publicacion): self
+    {
+        $this->fecha_publicacion = $fecha_publicacion;
+
+        return $this;
+    }
+
+    public function getContenido(): ?string
+    {
+        return $this->contenido;
+    }
+
+    public function setContenido(string $contenido): self
+    {
+        $this->contenido = $contenido;
+
+        return $this;
+    }
+}
